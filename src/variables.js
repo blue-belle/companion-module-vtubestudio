c519f1049f9714c0159594d7ea790138081ffa11
bluebelle <anderson.skye@hotmail.com>
2026-04-03 16:57:22 -0400

    Initial Commit

---
+++ src/variables.js
@@ -0,0 +1,7 @@
+module.exports = function (self) {
+	self.setVariableDefinitions([
+		{ variableId: 'variable1', name: 'Skoofs first variable' },
+		{ variableId: 'variable2', name: 'Skoofs second variable' },
+		{ variableId: 'variable3', name: 'Skoof another variable' },
+	])
+}
